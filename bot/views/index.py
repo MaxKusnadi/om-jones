c1670b15e02067ba9d355659663ef9e6926a2e1e
max <max.kusnadi@gmail.com>
2017-02-12 23:57:50 +0800

    update view

--- bot/views/index.py
+++ bot/views/index.py
@@ -1,6 +1,17 @@
+import logging
+import os
+
+from flask import request
 from bot import app
 
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/', methods=['GET'])
+def verify():
+    # when the endpoint is registered as a webhook, it must echo back
+    # the 'hub.challenge' value it receives in the query arguments
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+
+    return "Hello world", 200
